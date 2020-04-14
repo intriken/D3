1de5fe8b80a0fecb566b2a682b1e872026959ca7
Vincent Bellitto <c_vbellitto@groupon.com>
2020-04-14 14:48:04 -0700

    fix

--- Sankey/query.sql
+++ Sankey/query.sql
@@ -26,23 +26,23 @@ select server_sub_platform, traffic_source, sum(INSTANCEs) from test.vlb_sankey_
 drop table test.vlb_sankey_dvs;
 create table test.vlb_sankey_dvs
   STORED AS ORC tblproperties ("orc.compress" = "SNAPPY","auto.purge"="true") as
-select event_date, traffic_source, grt_l2_cat_description, sum(instances) instances, sum(cpv) cpv, sum(bbc) bbc
+select event_date, traffic_source, l2_cat_description, sum(instances) instances, sum(cpv) cpv, sum(bbc) bbc
 from (select event_date, cookie_first_traf_source traffic_source, deal_uuid, count(deal_uuid) instances, count(case when confirm_page_views <> 0 then deal_uuid end) cpv, count(case when buy_button_clicks <> 0 then deal_uuid end) bbc
       from user_testdw.traffic sf
       join test.vlb_sankey_consumers u on lower(sf.cookie_last_user_uuid) = u.consumer_id
       where event_date between '2018-10-01' and '2018-10-30'
       group by event_date, cookie_first_traf_source, deal_uuid
 ) sf
-join user_edwprod.dim_gbl_deal_lob dl on sf.deal_uuid = dl.deal_id
-group by event_date, traffic_source, grt_l2_cat_description;
+join deal_info dl on sf.deal_uuid = dl.deal_id
+group by event_date, traffic_source, l2_cat_description;
 
-select traffic_source, grt_l2_cat_description, sum(cpv) cpvs, sum(INSTANCEs) from test.vlb_sankey_dvs group by traffic_source, grt_l2_cat_description;
+select traffic_source, l2_cat_description, sum(cpv) cpvs, sum(INSTANCEs) from test.vlb_sankey_dvs group by traffic_source, l2_cat_description;
 
 
 
 create table test.vlb_sankey_orders
   STORED AS ORC tblproperties ("orc.compress" = "SNAPPY","auto.purge"="true") as
-select grt_l2_cat_description, order_date, count(distinct fgt.order_id) purchases
+select l2_cat_description, order_date, count(distinct fgt.order_id) purchases
 from test.transactions fgt
 join test.vlb_sankey_consumers u on user_uuid = u.consumer_id
 where fgt.region='na'
@@ -53,4 +53,4 @@ and lower(fgt.traffic_source)='email'
 and fgt.country_id in (235,40)
 and fgt.dt between '2018-10-01' and '2018-10-30'
 and fgt.brand = 'test'
-group by order_date, grt_l2_cat_description;
+group by order_date, l2_cat_description;
--- Session_Sequences_Sunburst/session_export_query.sql
+++ Session_Sequences_Sunburst/session_export_query.sql
@@ -1,10 +1,10 @@
 select concat_ws('|', flow) flows, count(*) instances
-from (select bcookie, collect_list(grt_l2_cat_name) flow
-      from (select bcookie, grt_l2_cat_description, row_number() over (partition by bcookie, dt order by session_start_time) session_row
+from (select bcookie, collect_list(l2_cat_name) flow
+      from (select bcookie, l2_cat_description, row_number() over (partition by bcookie, dt order by session_start_time) session_row
             from test.sessions
             where dt = '2019-02-10'
             and region = 'na'
-            and grt_l2_cat_description is not null
+            and l2_cat_description is not null
       ) a
       where session_row < 7
       group by bcookie
@@ -14,17 +14,14 @@ order by size(flow) desc
 ;
 
 select concat_ws('|', flow) flows, count(*) instances
-from (select consumer_id, collect_list(grt_l2_cat_description) flow, count(*) how_much
-      from (SELECT user_uuid consumer_id, grt_l2_cat_description, row_number() over (partition by user_uuid order by transaction_datetime) purchase_num
+from (select consumer_id, collect_list(l2_cat_description) flow, count(*) how_much
+      from (SELECT user_uuid consumer_id, l2_cat_description, row_number() over (partition by user_uuid order by transaction_datetime) purchase_num
             from test.transactions
             where region='na'
               and lower(transaction_type) ='order'
-              and platform_key = 1
-              and country_id  in (235,40)
               and dt >= '2018-01-01'
               and transaction_amount <> 0
-              and attribution_type = '3.1'
-              and grt_l2_cat_description is not null
+              and l2_cat_description is not null
     ) a
     where purchase_num <= 3
     group by consumer_id
@@ -37,34 +34,30 @@ group by flow
 drop table test.purchaser_flow purge;
 create table test.purchaser_flow
   STORED AS ORC tblproperties ("orc.compress" = "SNAPPY") as
-select purchase_num, division, grt_l2_cat_name, grt_l3_cat_name, last_grt_l2_cat_name, last_grt_l3_cat_name, avg(days_between) avg_days_between, count(*) purchases
-from (select consumer_id, division, order_datetime, last_order_datetime, (unix_timestamp(order_datetime) - unix_timestamp(last_order_datetime))/60/60/24 days_between, grt_l2_cat_name, last_grt_l2_cat_name, grt_l3_cat_name, last_grt_l3_cat_name, purchase_num
+select purchase_num, division, l2_cat_name, l3_cat_name, last_l2_cat_name, last_l3_cat_name, avg(days_between) avg_days_between, count(*) purchases
+from (select consumer_id, division, order_datetime, last_order_datetime, (unix_timestamp(order_datetime) - unix_timestamp(last_order_datetime))/60/60/24 days_between, l2_cat_name, last_l2_cat_name, l3_cat_name, last_l3_cat_name, purchase_num
       from (select user_uuid consumer_id,
               order_datetime,
               division,
-              grt_l2_cat_name,
-              grt_l3_cat_name,
+              l2_cat_name,
+              l3_cat_name,
               lag(order_datetime) over (partition by user_uuid order by order_datetime) last_order_datetime,
-              lag(grt_l2_cat_name) over (partition by user_uuid order by order_datetime) last_grt_l2_cat_name,
-              lag(grt_l3_cat_name) over (partition by user_uuid order by order_datetime) last_grt_l3_cat_name,
+              lag(l2_cat_name) over (partition by user_uuid order by order_datetime) last_l2_cat_name,
+              lag(l3_cat_name) over (partition by user_uuid order by order_datetime) last_l3_cat_name,
               row_number() over (partition by user_uuid order by order_datetime) purchase_num
-            from (select user_uuid, order_id, max(coalesce(deal_division_permalink, click_page_division, 'unknown')) division, max(grt_l2_cat_name) grt_l2_cat_name, max(grt_l3_cat_name) grt_l3_cat_name, min(order_datetime) order_datetime
+            from (select user_uuid, order_id, max(coalesce(deal_division_permalink, click_page_division, 'unknown')) division, max(l2_cat_name) l2_cat_name, max(l3_cat_name) l3_cat_name, min(order_datetime) order_datetime
             from test.transactions
             where region='na'
               and dt >= '2018-01-01'
-              and attribution_type = '3.1'
               and lower(transaction_type) ='order'
-              and platform_key = 1
-              and country_id  in (235,40)
               and transaction_amount <> 0
-              and grt_l2_cat_name is not null
-              and grt_l3_cat_name is not null
+              and l2_cat_name is not null
+              and l3_cat_name is not null
               and action = 'authorize'
               and user_uuid is not null
-              -- and user_uuid = '5e584250-eb46-11e7-b61b-5254198b31e0'
               group by user_uuid, order_id
             ) f
       ) f
 ) f
-group by division, purchase_num, grt_l2_cat_name, last_grt_l2_cat_name, grt_l3_cat_name, last_grt_l3_cat_name
+group by division, purchase_num, l2_cat_name, last_l2_cat_name, l3_cat_name, last_l3_cat_name
 sort by purchase_num, purchases desc;
